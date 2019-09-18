ca34133306dfa1b67fe57be66c6c88a21b20604d
adharsh8 <adharsh.s@psiog.com>
2019-09-18 18:34:40 +0530

    Update login.js

--- script/login.js
+++ script/login.js
@@ -1,29 +1,60 @@
-var username= null;
-var password = null;
-var repassword = null;
+
+var userarray=[];
+var passarray=[];
+var repassarray=[];
+var storeuser="";
+var storepsw="";
 
 function store()
 {
-    username=document.getElementById("user").value;
-    password=document.getElementById("password").value;
-    repassword=document.getElementById("repassword").value;
-    localStorage.setItem("user",username);
-    localStorage.setItem("password",password);
-    localStorage.setItem("repassword",repassword);
+    if(window.localStorage["user"]!=null)
+        userarray=JSON.parse(window.localStorage["user"]);
+        userarray.push(document.getElementById("user").value);
+        window.localStorage["user"]=JSON.stringify(userarray);
+
+    if(window.localStorage["pass"]!=null)
+        passarray=JSON.parse(window.localStorage["pass"]);
+        passarray.push(document.getElementById("password").value);
+        window.localStorage["pass"]=JSON.stringify(passarray);
+    
+    if(window.localStorage["repass"]!=null)
+        repassarray=JSON.parse(window.localStorage["repass"]);
+        repassarray.push(document.getElementById("repassword").value);
+        window.localStorage["repass"]=JSON.stringify(repassarray);
+
+
     alert("Succesfully Signed Up");
 }
 
    
 function check()
 {
-    var storeuser=localStorage.getItem("user", username);
-    var storepsw=localStorage.getItem("password",password);
+    storeuser=JSON.parse(localStorage.getItem("user"));
+    console.log(storeuser);
+    storepsw=JSON.parse(localStorage.getItem("pass"));
+    console.log(storepsw);
     var loguser=document.getElementById("loguser").value;
     var logpsw=document.getElementById("logpsw").value;
-    
-    if((storeuser==loguser)&&(storepsw==logpsw))
+    var flag=0;
+    if((loguser!="")&&(logpsw!=""))
+    { 
+        for(var i=0;i<storeuser.length;i++)
+        {
+    if((storeuser[i]==loguser)&&(storepsw[i]==logpsw))
     {
+        flag =1;
+       
+    }
+    else{
+        flag=0;
+      
+    }
+    }
+    }
+    if(flag=1){
         alert("You have logged in Successfully")
+        document.getElementById("loguser").value="";
+        document.getElementById("logpsw").value="";
         self.location="file:///C:/Users/adharsh.s/Documents/GitHub/Task-3/source/index.html";
     }
     else{
